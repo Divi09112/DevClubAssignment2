b7b2225ae20e14d9a0600910a45b4cb82a80f248
Divyansh <divi09112@gmail.com>
2017-12-09 12:17:33 +0530

    more readable code

--- JS1/js1.js
+++ JS1/js1.js
@@ -1,36 +1,42 @@
-let k=0;
+var k = 0;
 
-//for adding a task
-function add() {
-  let text = document.getElementById('task').value;
+var dark = true;
 
-  if (text !== '') {
-    let li =document.createElement('li');
-    li.appendChild(document.createTextNode(text));
-    li.setAttribute("id",k);
-    li.setAttribute('onClick',`strikeout(${k})`);
-    k++;
-    list=document.getElementById('list');
-    list.appendChild(li);
-
-    let hr =document.createElement('hr');
-    list.appendChild(hr);
-
-    if (list.clientHeight>=150){
-        list.setAttribute('overflow','auto');
+function enter(e){
+  if (e.keyCode==13){
+    var li = document.createElement("li");
+    li.innerHTML=document.getElementById("entry").value;
+    li.setAttribute("Id", "task"+k);
+    document.getElementById("list").appendChild(li);
+    
+    if (dark){
+      li.className="dark";
+      dark = false;
+    }
+    else{
+      li.className="light";
+      dark = true;
     };
+    
+    li.setAttribute('onClick',`strike(${k})`);
+    k++;
   };
-  document.getElementById('task').value='';
 }
 
-function strikeout(n) {
 
-  let el = document.getElementById(n.toString());
+function strike(n){
+  var el = document.getElementById("task"+n);
 
-  if(el.className === ''){
-    el.setAttribute("class",'clicked');
+  if (el.className !== "clicked"){
+    el.className="clicked";
   }
   else{
-    el.setAttribute("class",'');
+    const val = el.id.slice(4,el.id.length);
+    if (val%2===0) {
+      el.className = "dark";
+    }
+    else{
+      el.className = "light";
+    };
   };
 }
